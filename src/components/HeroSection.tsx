2c9ff02cfd0556b554c6f53352c9f32f9b51c6a5
SkyServices <srkr0990@gmail.com>
2026-01-18 15:26:38 +1300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,8 +19,8 @@ export function HeroSection() {
   style={{
     backgroundImage: `
       linear-gradient(
-        rgba(0, 0, 0, 0.55),
-        rgba(0, 0, 0, 0.55)
+        rgba(0, 0, 0, 0.75),
+        rgba(0, 0, 0, 0.75)
       ),
       url(${heroBg})
     `,
